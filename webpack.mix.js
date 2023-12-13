5069df6b2f149d9f7c76a65a29ff2f51d2859af1
bellaqamendim@gmail.com <bellaqamendim@gmail.com>
2023-12-13 03:18:31 +0100

    `webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
- mix.js('resources/js/vueapp.js', 'public/js')
- .vue()
- .version()
+ mix.js('resources/js/app.js', 'public/js').vue()
  .sass('resources/sass/app.scss', 'public/css');
+
+
+
+ 
